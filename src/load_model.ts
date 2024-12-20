55999032413fcf40059161b7adfa57ba2f738d37
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2024-12-21 01:21:09 +0900

    Refactor

--- src/load_model.ts
+++ src/load_model.ts
@@ -2,16 +2,12 @@ import * as THREE from 'three';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-export interface CustomModel {
-  position: Float32Array;
-  normal: Float32Array;
-  uv: Float32Array;
-}
+import { StaticModel } from './types';
 
-export async function load_obj(url: string): Promise<CustomModel> {
+export async function load_obj(model_url: string, texture_url?: string): Promise<StaticModel> {
   const loader = new OBJLoader();
   const root: THREE.Group = await new Promise((resolve, reject) => {
-    loader.load(url, resolve, undefined, reject);
+    loader.load(model_url, resolve, undefined, reject);
   });
   if (!root) {
     throw new Error('Failed to load OBJ');
@@ -22,15 +18,20 @@ export async function load_obj(url: string): Promise<CustomModel> {
   const position = (obj.geometry as THREE.BufferGeometry).attributes.position.array;
   const normal = (obj.geometry as THREE.BufferGeometry).attributes.normal.array;
   const uv = (obj.geometry as THREE.BufferGeometry).attributes.uv.array;
+  // Get texture from URL if it exists, otherwise use a blank canvas
+  const texture = texture_url
+    ? new THREE.TextureLoader().load(texture_url)
+    : new THREE.CanvasTexture(new HTMLCanvasElement());
 
   return {
     position: new Float32Array(position),
     normal: new Float32Array(normal),
     uv: new Float32Array(uv),
+    texture: texture.image as HTMLCanvasElement,
   };
 }
 
-export async function load_gltf(url: string): Promise<CustomModel> {
+export async function load_gltf(url: string): Promise<StaticModel> {
   const loader = new GLTFLoader();
   const root: THREE.Group = await new Promise((resolve, reject) => {
     loader.load(
@@ -51,10 +52,13 @@ export async function load_gltf(url: string): Promise<CustomModel> {
   const position = (obj.geometry as THREE.BufferGeometry).attributes.position.array;
   const normal = (obj.geometry as THREE.BufferGeometry).attributes.normal.array;
   const uv = (obj.geometry as THREE.BufferGeometry).attributes.uv.array;
+  // Get texture from material if it exists, otherwise use a blank canvas
+  const texture = (obj.material as THREE.MeshBasicMaterial).map || new THREE.CanvasTexture(new HTMLCanvasElement());
 
   return {
     position: new Float32Array(position),
     normal: new Float32Array(normal),
     uv: new Float32Array(uv),
+    texture: texture.image as HTMLCanvasElement,
   };
 }
---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface StaticModel {
+  position: Float32Array;
+  normal: Float32Array;
+  uv: Float32Array;
+  texture: HTMLCanvasElement;
+}
+
+export interface RenderableModel {
+  staticModel: StaticModel;
+  location: Float32Array;
+  rotation: Float32Array;
+  scale: Float32Array;
+}
+
+export interface Camera {
+  location: Float32Array;
+  direction: Float32Array;
+  fov: number;
+  aspectRatio: number;
+}
