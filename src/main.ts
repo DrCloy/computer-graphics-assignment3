170729919a6d7f86a2bcb88065cb3059faa8cf56
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2024-12-20 20:40:32 +0900

    Set project

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    
+    <h1 id="header">Project #3: Tank</h1>
+    <canvas id="canvas"></canvas>
+    <div id="message">Message Area</div>
   </div>
 `;
