95694d36450666e0f4a3294047cd1b6a0ec7ae59
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2024-12-21 16:01:23 +0900

    Fix index buffer error

--- src/load_model.ts
+++ src/load_model.ts
@@ -51,18 +51,37 @@ export async function load_gltf(url: string): Promise<StaticModel[]> {
     }
   });
 
+  console.log(meshes);
+
   return meshes.map((mesh) => {
     const name = mesh.name;
     const position = (mesh.geometry as THREE.BufferGeometry).attributes.position.array;
     const normal = (mesh.geometry as THREE.BufferGeometry).attributes.normal.array;
     const uv = (mesh.geometry as THREE.BufferGeometry).attributes.uv.array;
     const texture = (mesh.material as THREE.MeshStandardMaterial).map?.image as HTMLCanvasElement;
+    const index = (mesh.geometry as THREE.BufferGeometry).index?.array;
+
+    const indexedPosition = [] as number[];
+    const indexedNormal = [] as number[];
+    const indexedUV = [] as number[];
+
+    if (index) {
+      for (let i = 0; i < index.length; i++) {
+        indexedPosition.push(position[index[i] * 3], position[index[i] * 3 + 1], position[index[i] * 3 + 2]);
+        indexedNormal.push(normal[index[i] * 3], normal[index[i] * 3 + 1], normal[index[i] * 3 + 2]);
+        indexedUV.push(uv[index[i] * 2], uv[index[i] * 2 + 1]);
+      }
+    } else {
+      indexedPosition.push(...position);
+      indexedNormal.push(...normal);
+      indexedUV.push(...uv);
+    }
 
     return {
       name,
-      position: new Float32Array(position),
-      normal: new Float32Array(normal),
-      uv: new Float32Array(uv),
+      position: new Float32Array(indexedPosition),
+      normal: new Float32Array(indexedNormal),
+      uv: new Float32Array(indexedUV),
       texture,
     };
   });
